adea3db95e5da166aa1c2ff5fb0b109dd52f31ee
Dmitry Baryshkov <dbaryshkov@gmail.com>
2006-03-25 23:41:49 +0000

    a tiny bit of documentation.

--- src-model/org/jbookreader/book/stylesheet/IStyleSheet.java
+++ src-model/org/jbookreader/book/stylesheet/IStyleSheet.java
@@ -18,7 +18,22 @@ public interface IStyleSheet {
 	 */
 	EDisplayType getNodeDisplayType(INode node);
 	
+	/**
+	 * Returns the left margin for the node.
+	 * @param node the node to get syuling information for
+	 * @return the left margin for the node.
+	 */
 	double getLeftMargin(INode node);
+	/**
+	 * Returns the left margin for the first line of the node.
+	 * @param node the node to get syuling information for
+	 * @return the left margin for the first line of the node.
+	 */
 	double getFirstLineMargin(INode node);
+	/**
+	 * Returns the right margin for the node.
+	 * @param node the node to get syuling information for
+	 * @return the right margin for the node.
+	 */
 	double getRightMargin(INode node);
 }
